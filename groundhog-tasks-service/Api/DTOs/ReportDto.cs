45d9edca23d13c3137cf5cdbc78e5a3213010ab5
miguelrivasmorantes <miguelrivasmorantes@gmail.com>
2025-11-30 21:09:56 +0100

    feature/create CRUD for reports

---
+++ groundhog-tasks-service/Api/DTOs/ReportDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace groundhog_tasks_service.Api.DTOs
+{
+    public class ReportDto
+    {
+        public Guid? Id { get; set; }
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public Guid AssignmentId { get; set; }
+
+        public bool Problems { get; set; } = false;
+
+        public string? Description { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+    }
+}
